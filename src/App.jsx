05455de2044b1d370f142378e532c16c2dc57405
AhmedIZekry <ahmediizekry@gmail.com>
2026-01-09 07:34:42 +0200

    Add products as front end objects and making the app act like SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import {Routes,Route} from 'react-router';
 import './App.css';
 import {HomePage} from "./pages/HomePage.jsx";
+import {Checkout} from "./pages/Checkout.jsx";
+import {Orders} from "./pages/Orders.jsx";
+import {Tracking} from "./pages/Tracking.jsx";
 
 function App() {
 
 
   return (
       <Routes>
-        <Route index element={HomePage} />
+          <Route index element={<HomePage/>}/>
+          <Route path="/checkout" element={<Checkout/>}/>
+          <Route path="/orders" element={<Orders />}/>
+          <Route path="/tracking" element={<Tracking />}/>
       </Routes>
 
   )
